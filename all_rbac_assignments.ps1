63da585a3a4b40484efee5f2c0886e816fbdbe11
Victoria <texnokot@users.noreply.github.com>
2025-04-07 11:40:26 +0200

    Update all_rbac_assignments.ps1

--- all_rbac_assignments.ps1
+++ all_rbac_assignments.ps1
@@ -1,7 +1,7 @@
-# Script to collect all role assignments across subscriptions where you have permissions
 # Connect to Azure and Microsoft Graph
 $startTime = Get-Date
-Connect-MgGraph -Scopes "User.Read.All","Group.Read.All"
+Connect-MgGraph -Scopes "Directory.Read.All","User.Read.All","Group.Read.All","Application.Read.All"
+Connect-AzAccount
 
 $outputFile = "RBAC_Assignments_$(Get-Date -Format 'yyyy-MM-dd_HH-mm-ss').csv"
 $results = [System.Collections.Generic.List[object]]::new()
@@ -9,43 +9,74 @@ $subscriptions = Get-AzSubscription -WarningAction SilentlyContinue
 $totalSubs = $subscriptions.Count
 $currentSub = 0
 
-# Process subscriptions with progress tracking
 foreach ($sub in $subscriptions) {
     $currentSub++
     $percentComplete = ($currentSub / $totalSubs) * 100
     
     Write-Progress -Activity "Processing Subscriptions" -Status "$currentSub/$totalSubs ($([math]::Round($percentComplete,1))%)" `
         -CurrentOperation $sub.Name -PercentComplete $percentComplete
-    
-    # Get and process role assignments
+
+    Set-AzContext -Subscription $sub.Id | Out-Null
     $roleAssignments = Get-AzRoleAssignment -ErrorAction SilentlyContinue
+
+    # Batch lookup missing identities
+    $missingIdentityIds = $roleAssignments | 
+        Where-Object { [string]::IsNullOrEmpty($_.DisplayName) } | 
+        Select-Object -ExpandProperty ObjectId -Unique
+    
+    $identityMap = @{}
+    if ($missingIdentityIds) {
+        Get-MgDirectoryObjectById -Ids $missingIdentityIds -ErrorAction SilentlyContinue | ForEach-Object {
+            $objectType = $_.AdditionalProperties['@odata.type'].Split('.')[-1]
+            
+            # Handle Managed Identity detection
+            if ($objectType -eq "ServicePrincipal") {
+                $sp = Get-MgServicePrincipal -ServicePrincipalId $_.Id -Property "servicePrincipalType,appId" -ErrorAction SilentlyContinue
+                if ($sp.ServicePrincipalType -eq "ManagedIdentity" -or $sp.AppId -match "managedidentity") {
+                    $objectType = "ManagedIdentity"
+                }
+            }
+            
+            $identityMap[$_.Id] = @{
+                DisplayName = $_.AdditionalProperties['displayName']
+                UserPrincipalName = $_.AdditionalProperties['userPrincipalName']
+                ObjectType = $objectType
+            }
+        }
+    }
+
     foreach ($ra in $roleAssignments) {
         $displayName = $ra.DisplayName
         $signInName = $ra.SignInName
-        
+        $objectType = $ra.ObjectType
+
         if ([string]::IsNullOrEmpty($displayName)) {
-            switch ($ra.ObjectType) {
-                "User" { 
-                    $user = Get-MgUser -UserId $ra.ObjectId -ErrorAction SilentlyContinue
-                    $displayName = $user?.DisplayName
-                    $signInName = $user?.UserPrincipalName
-                }
-                "Group" {
-                    $group = Get-MgGroup -GroupId $ra.ObjectId -ErrorAction SilentlyContinue
-                    $displayName = $group?.DisplayName
-                }
-                "ServicePrincipal" {
-                    $sp = Get-MgServicePrincipal -ServicePrincipalId $ra.ObjectId -ErrorAction SilentlyContinue
-                    $displayName = $sp?.DisplayName
+            $lookup = $identityMap[$ra.ObjectId]
+            if ($lookup) {
+                $displayName = $lookup.DisplayName
+                $objectType = $lookup.ObjectType
+                $signInName = if ($lookup.UserPrincipalName) { $lookup.UserPrincipalName } else { $signInName }
+            }
+        }
+
+        # Final object type normalization
+        $objectType = switch ($objectType) {
+            "ServicePrincipal" { 
+                $sp = Get-MgServicePrincipal -ServicePrincipalId $ra.ObjectId -Property "servicePrincipalType" -ErrorAction SilentlyContinue
+                if ($sp.ServicePrincipalType -eq "ManagedIdentity") {
+                    "ManagedIdentity"
+                } else {
+                    "ServicePrincipal"
                 }
             }
+            default { $objectType }
         }
 
         $results.Add([PSCustomObject]@{
             SubscriptionId      = $sub.Id
             SubscriptionName    = $sub.Name
             ObjectId            = $ra.ObjectId
-            ObjectType          = $ra.ObjectType
+            ObjectType          = $objectType
             RoleDefinitionName  = $ra.RoleDefinitionName
             Scope               = $ra.Scope
             DisplayName         = $displayName
@@ -54,13 +85,10 @@ foreach ($sub in $subscriptions) {
     }
 }
 
-# Export results
 $results | Export-Csv -Path $outputFile -NoTypeInformation -Encoding UTF8
 Write-Progress -Activity "Processing Subscriptions" -Completed
 
-# Calculate and display execution time
-$endTime = Get-Date
-$executionTime = $endTime - $startTime
-Write-Host "`nScript completed in $($executionTime.ToString('hh\:mm\:ss')) (hh:mm:ss)"
+$executionTime = (Get-Date) - $startTime
+Write-Host "`nScript completed in $($executionTime.ToString('hh\:mm\:ss'))"
 Write-Host "Results saved to: $outputFile"
 Write-Host "Total role assignments processed: $($results.Count)"
